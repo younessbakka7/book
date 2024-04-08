439e8b000eb602eaeda8d39278efac2e9c0f0cbb
younessbakka7 <younessbakka7@gmail.com>
2024-04-08 23:24:33 +0000

    page about-and-contact

--- bibstore/src/Components/pages/About/About.jsx
+++ bibstore/src/Components/pages/About/About.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-
+import  "./About.css"
 const About = () => {
     return (
-        <div>
-           About 
+        <div className='about'>
+         <div className='about-us'>
+            <h2 className='title-about'>About Us</h2>
+            <p className='texte-about'>we are a online book store</p>
+            <h3 className="version-about">Version 1.0.0.0</h3>
+         </div>
+           
         </div>
     );
 }
--- bibstore/src/Components/pages/Contact/Contact.jsx
+++ bibstore/src/Components/pages/Contact/Contact.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
-
+import './Contact.css'
 const Contact = () => {
     return (
-        <div>
-            Contact
+        <div className='contact'>
+
+            <div className='contact-wrapper'>
+
+                <div className='contact-item'>
+                    <div className='contact-item-icon'>
+                        <i class="bi bi-house-fill"></i>
+                    </div>
+                    <div className='contact-item-info'>
+                        <h2 className='contact-item-title'>Address</h2>
+
+                    </div>
+                    <p className='contact-item-desc'>Marroc-Marrakech</p>
+                </div>
+
+                <div className='contact-item'>
+                    <div className='contact-item-icon'>
+                        <i class="bi bi-telephone-fill"></i>
+                    </div>
+                    <div className='contact-item-info'>
+                        <h2 className='contact-item-title'>Phone</h2>
+
+                    </div>
+                    <p className='contact-item-desc'>+212 617 03 94 18</p>
+                </div>
+
+                <div className='contact-item'>
+                    <div className='contact-item-icon'>
+                        <i class="bi bi-envelope-fill"></i>
+                    </div>
+                    <div className='contact-item-info'>
+                        <h2 className='contact-item-title'>Email</h2>
+
+                    </div>
+                    <p className='contact-item-desc'>younessbakka7@gmail.com</p>
+                </div>
+
+            </div>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+            }} className='form-contact'>
+                <h2 className='contact-title-form'>Contact Us Form</h2>
+                <div className='form-input'>
+                <input  type="text" placeholder='Nom *' />
+                <input type="text" placeholder='Prenom *' />
+                <input type="text" placeholder='Email *'  />
+                </div>
+           
+             <div className='form-input-aria'>
+               <textarea  cols="30" rows="10" placeholder='Comment'></textarea>
+               <button className='btn-contact'>Send</button>
+                </div>
+           
+
+            </form>
+
         </div>
     );
 }
